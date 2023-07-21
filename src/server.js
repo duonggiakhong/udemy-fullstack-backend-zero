a0b9ad9183eff543945c5dfa7feb2e92537290de
duonggiakhong <20211tt2616@mai.tdc.edu.vn>
2023-07-22 03:15:34 +0700

    #19 ENV

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 
+require('dotenv').config()
 const app = express()
-const port = 8081
+const port = process.env.POST;
+const hostname = process.env.HOST_NAME;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -16,6 +18,6 @@ app.get('/giacat', (req, res) => {
     res.render('sample.ejs');
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
 })
