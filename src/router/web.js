59a9ce0bc7cec67c93db963762ef22db47ef38b7
duonggiakhong <20211tt2616@mai.tdc.edu.vn>
2023-07-24 01:07:45 +0700

    #24 MVC past 1

---
+++ src/router/web.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+router.get('/sample', (req, res) => {
+    res.render('sample.ejs');
+})
+
+module.exports = router;// export default
--- src/server.js
+++ src/server.js
@@ -1,22 +1,19 @@
 const express = require('express')
 const path = require('path')
+const configViewEngine = require('./config/viewEngine');
+
+const webRouter = require('./router/web');
 
 require('dotenv').config()
 const app = express()
 const port = process.env.POST;
 const hostname = process.env.HOST_NAME;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+//config viewEngine 
+configViewEngine(app);
 
-
-app.set('views', path.join(__dirname, 'views'));//nơi lưu trữ 
-app.set('view engine', 'ejs');
-
-app.get('/giacat', (req, res) => {
-    res.render('sample.ejs');
-})
+//khai bao router
+app.use('/', webRouter);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
